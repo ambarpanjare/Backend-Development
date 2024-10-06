7a2b876548a5e654a54117640acaa16e66aca222
Ambar Panjare <ambarpanjare@gmail.com>
2024-10-06 12:26:46 +0530

    register functionality added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async(req,res)=>{
     // return res
     
     const {fullName,email,username,password}=req.body
-    console.log("email : ",email);
+    //console.log("email : ",email);
 
     // validation- not empty
     if(
@@ -27,31 +27,40 @@ const registerUser = asyncHandler(async(req,res)=>{
     } 
 
     // check if user already exists : username,email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or:[{ username },{ email }]
     })
     if(existedUser){
         throw new ApiError(409,"User already exists")
     }
+    
+    
+    ///console.log(req.files);
 
-    const avtarLocalPath = req.files?.avtar[0]?.path
-    const coverImageLocalPath = req.files?.coverImage[0]?.path
+    const avatarLocalPath = req.files?.avatar[0]?.path
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path
 
-    if(!avtarLocalPath){
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
+
+
+    if(!avatarLocalPath){
         throw new ApiError(400,"Avtar is required")
     }
 
     // upload them to cloudnary, avtar
-    const avtar = await uploadOnCloudinary(avtarLocalPath)
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if(!avtar){
+    if(!avatar){
         throw new ApiError(400,"Failed to upload avtar")
     }
     // create user object- create entry in db
     const user =await User.create({
         fullName,
-        avtar:avtar.url,
+        avatar:avatar.url,
         coverImage:coverImage?.url || "",
         email,
         password,
