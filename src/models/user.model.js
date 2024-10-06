663c47929e5dcf60b2427bfdfbc2c24c55d03aa3
Ambar Panjare <ambarpanjare@gmail.com>
2024-10-06 11:04:31 +0530

    bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -33,7 +33,7 @@ const userSchema=new Schema({
     },
     watchHistory:[
         {
-            typr:Schema.types.ObjectId,
+            type:Schema.Types.ObjectId,
             ref:"Video"
         }
     ],
